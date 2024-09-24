8ae3ebb4b03ba7d8d0a7616606644ee539c36aa7
jessicacorrea <jessicamilenacorrea@gmail.com>
2024-09-24 20:58:23 -0300

    first commit

---
+++ app/src/main/java/br/com/jessica/productcatalog/MainActivity.kt
@@ -0,0 +1,92 @@
+package br.com.jessica.productcatalog
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.jessica.productcatalog.ui.theme.ProductCatalogTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProductCatalogTheme {
+                Surface {
+                    MyColumn()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyColumn() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text("Banana", modifier = Modifier.background(Color.Yellow))
+        Text("Laranja", modifier = Modifier.background(Color.Blue))
+        Text("Maçã", modifier = Modifier.background(Color.Red))
+        Text("Manga", modifier = Modifier.background(Color.Cyan))
+        Text("Uva", modifier = Modifier.background(Color.Magenta))
+        Text(
+            "Manga", modifier = Modifier
+                .background(Color.Cyan)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Uva", modifier = Modifier
+                .background(Color.Magenta)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+    }
+}
+
+@Composable
+fun MyBox() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Box(
+            modifier = Modifier
+                .background(Color.Cyan)
+                .width(200.dp)
+                .height(200.dp)
+                .verticalScroll(
+                    rememberScrollState()
+                ), contentAlignment = Alignment.Center
+        ) {
+            Text("Isto é apenas um exemplo")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ProductCatalogTheme {
+        MyColumn()
+    }
+}
