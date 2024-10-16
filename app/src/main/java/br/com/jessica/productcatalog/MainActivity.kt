10bcec376459462c7fe78c96dcb6dfc011fc9d17
jessicacorrea <jessicamilenacorrea@gmail.com>
2024-10-15 21:06:46 -0300

    Usando states

--- app/src/main/java/br/com/jessica/productcatalog/MainActivity.kt
+++ app/src/main/java/br/com/jessica/productcatalog/MainActivity.kt
@@ -17,9 +17,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -33,10 +40,25 @@ class MainActivity : ComponentActivity() {
         setContent {
             ProductCatalogTheme {
                 Surface {
-                    MyComplexLayout()
+                    MyStateExample()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MyStateExample() {
+    var counter by rememberSaveable { mutableIntStateOf(0) }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { counter += 1 }) {
+            Text(text = "Enviar")
+        }
+        Text(text = "Foi eviado counter  vezes ")
     }
 }
 
@@ -162,6 +184,6 @@ fun MyBox() {
 @Composable
 fun GreetingPreview() {
     ProductCatalogTheme {
-        MyComplexLayout()
+        MyStateExample()
     }
 }
